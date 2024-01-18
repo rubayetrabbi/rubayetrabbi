408d4f0c197703d60fbbfc11388e28cee83a29f6
S M Riad Hossain <riadhossain658@gmail.com>
2024-01-18 21:06:07 +0600

    story read more added

--- script.js
+++ script.js
@@ -4,3 +4,51 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+function readMore() {
+  var dots = document.getElementById("dots-1");
+  var moreText = document.getElementById("more-story1");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
+
+function readMore2() {
+  var dots = document.getElementById("dots-2");
+  var moreText = document.getElementById("more-story2");
+  var btnText = document.getElementById("myBtn-2");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
+
+function readMore3() {
+  var dots = document.getElementById("dots-3");
+  var moreText = document.getElementById("more-story3");
+  var btnText = document.getElementById("myBtn-3");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
